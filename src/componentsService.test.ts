3ca5618b67e970653dc583603f322fc577a5c3a6
Nick Preddy <6988369+njpreddy@users.noreply.github.com>
2026-09-21 15:09:23 +0100

    Import removed

--- src/componentsService.test.ts
+++ src/componentsService.test.ts
@@ -5,7 +5,6 @@ import getFrontendComponents from './componentsService'
 import * as UpdateCspModule from './utils/updateCsp'
 import ComponentApiClientModule from './data/componentApi/componentApiClient'
 import { fakeLogger } from '../test/helpers/loggerStub'
-import { sanitizeComponentHtml } from './utils/sanitizeHtml'
 
 const apiResponse = {
   header: { html: 'header', css: ['header.css'], javascript: ['header.js'] },
